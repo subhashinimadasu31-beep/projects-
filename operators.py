d0b62dd49c6ac9f5b44eb4c6fd92f186e1add1df
subhashinimadasu31-beep <subhashinimadasu31@gmail.com>
2026-06-05 11:38:45 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,24 @@
+snack_name= "chips"
+price=1.50
+quantity=10
+_is_available=True
+print("snacks:",snack_name)
+print('price:',price)
+print('in stock:',quantity)
+print('available:',_is_available)
+print(type(snack_name))
+print(type(price))
+print(type(quantity))
+print(type(_is_available))
+a=quantity*price
+b=price-0.25
+c=quantity*2
+print(price<2)
+print(quantity>5)
+print(price==1.50)
+a=2
+b=3
+c=a
+a=b
+b=c
+print(a,b)
---
+++ prints.py
@@ -0,0 +1,11 @@
+print(" welcome to the quizz game")
+print("how many states are there in india ?")
+print("1. 30")
+print("2. 24")
+print("3. 28")
+print("Enter your choice :")
+a = input()
+if a == "3":
+   print ("correct!")
+else: 
+   print("incorrect!")
