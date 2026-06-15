a12351391ae3a40f33768e1f14298bc13544dd68
subhashinimadasu31-beep <subhashinimadasu31@gmail.com>
2026-06-15 11:58:48 +0530

    Add files via upload

---
+++ animation.py
@@ -0,0 +1,9 @@
+import turtle 
+turtle.Screen().bgcolor("blue")
+B= turtle.Screen()
+B.setup(500,400)
+turtle.title("welcome to turtle lib")
+a = turtle.Turtle()
+for i in range (4):
+    a.forward(100)
+    a.left(20)
