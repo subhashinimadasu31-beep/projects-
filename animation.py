01ccb152c7bf1c7cfd5dda98ac97be9dd544b2ec
subhashinimadasu31-beep <subhashinimadasu31@gmail.com>
2026-06-19 12:39:37 +0530

    Add files via upload

--- animation.py
+++ animation.py
@@ -1,9 +1,35 @@
-import turtle 
-turtle.Screen().bgcolor("blue")
-B= turtle.Screen()
-B.setup(500,400)
-turtle.title("welcome to turtle lib")
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("lightblue")
+screen.setup(500,400)
+screen.title("Welcome to turtle lib")
 a = turtle.Turtle()
-for i in range (4):
+
+# Triangle
+for i in range (3):
     a.forward(100)
-    a.left(90)
+    a.left(120)
+
+    # Move to next shape
+    for i in range (4):
+        a.forward(200)
+        a.left(90)
+        a.forward(60)
+        a.left(90)
+
+        # hexangon 
+        for i in range (6):
+            a.forward(80)
+            a.left(60)
+
+            turtle.done()
+
+
+
+
+
+
+
+
+
