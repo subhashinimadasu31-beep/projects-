cc2bb766d44309b21202fe732255a77f8dfa5afa
subhashinimadasu31-beep <subhashinimadasu31@gmail.com>
2026-08-25 19:44:39 +0530

    Add files via upload

---
+++ mathmagic2.py
@@ -0,0 +1,8 @@
+def print_factors(number):
+    print("the factors of", number,"are:")
+    for i in range(1,number + 1 )
+    if number % i == 0 
+    print(i)
+    number = int("enter your number to know its factors: ")
+    print_factors(number)
+    
