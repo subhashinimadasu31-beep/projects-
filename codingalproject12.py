e558a58002cc1bf924406c4cc0b153e2f441d77c
subhashinimadasu31-beep <subhashinimadasu31@gmail.com>
2026-09-08 20:37:42 +0530

    Add files via upload

---
+++ codingalproject12.py
@@ -0,0 +1,9 @@
+import math 
+
+largest = int(input("Enter your largest number:"))
+smallest = int(input("Enter your smallest number:"))
+
+result = math.lcm( largest, smallest)
+
+print(f"LCM is {result}" )
+
